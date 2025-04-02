86c75cafc7086466d721e2df6c5c4e85854ee18c
MohamedRouias <146641169+MohamedRouias@users.noreply.github.com>
2025-04-02 19:07:14 +0000

    DomainsGenerator Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+function DomainGenerator() {
+
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  //freestyle
+  let domains = [".com", ".es", ".net"];
+
+  for (let pronoun in pronouns) {
+    for (let adj in adjs) {
+      for (let noun in nouns) {
+        console.log(pronouns[pronoun] + adjs[adj] + nouns[noun] + ".com");
+      }
+    }
+  }
+}
+DomainGenerator();
+
